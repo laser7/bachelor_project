99685a8a7822b5d04d872fe021473a8d4d19d751
laser7 <laser556678@gmail.com>
2019-11-22 17:48:39 +0100

    car added

---
+++ js/car.js
@@ -0,0 +1,90 @@
+var stage;
+function init(){
+    stage = new createjs.Stage("canvas");
+
+    var park = new createjs.Shape();
+    park.graphics.beginFill("white").drawRect(0, 0, 810, 30);
+    park.x = 200;
+    park.y = 60;
+    var park1 = new createjs.Shape();
+    park1.graphics.beginFill("grey").drawRect(0, 0, 170, 240);
+    park1.x = 240;
+    park1.y = 90;
+    var park2 = new createjs.Shape();
+    park2.graphics.beginFill("grey").drawRect(0, 0, 170, 240);
+    park2.x = 430;
+    park2.y = 90;
+    var park3 = new createjs.Shape();
+    park3.graphics.beginFill("grey").drawRect(0, 0, 170, 240);
+    park3.x = 620;
+    park3.y = 90;
+    var park4= new createjs.Shape();
+    park4.graphics.beginFill("grey").drawRect(0, 0, 170, 240);
+    park4.x = 810;
+    park4.y = 90;
+    var park5 = new createjs.Shape();
+    park5.graphics.beginFill("white").drawRect(0, 0, 810, 30);
+    park5.x = 200;
+    park5.y = 330;
+
+
+    var man = new createjs.Bitmap("/img/man.png");
+    man.x = 1200;
+    man.y = 550;
+    man.addEventListener("click",function(e){
+    });
+
+
+    var img = new Image();
+    img.src = "/img/cartop.png";
+    var car = new createjs.Bitmap(img);
+    car.x = 400;
+    car.y = 700;
+   
+    window.addEventListener('keydown',keyOper, true);
+    function keyOper(e){
+        var keyID = e.keyCode ? e.keyCode :e.which;
+        if(keyID === 37){
+            img.src = "/img/carleft.png";
+            car.x -= 30;
+            checkPosition(car.x,car.y);
+        } else if(keyID === 38){
+            img.src = "/img/cartop.png";
+            car.y -= 30;
+            checkPosition(ar.x,car.y);
+        } else if(keyID === 39){
+            img.src = "/img/carright.png";
+            car.x += 30;
+            checkPosition(ar.x,car.y);
+        } else if (keyID === 40){
+            img.src = "/img/cardown.png";
+            car.y += 30;
+            checkPosition(ar.x,car.y);
+        }
+    }
+
+    function checkPosition(a,b){
+        if((910< a < 1080) && (90<b< 400)){
+            swal("super gemacht!", "du hast richtig geparkt","success");
+        }
+    }
+
+    stage.addChild(park);
+    stage.addChild(park5);
+    stage.addChild(park1);
+    stage.addChild(park2);
+    stage.addChild(park3);
+    stage.addChild(park4);
+    stage.addChild(man);
+    stage.addChild(car);
+
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener('tick',update);
+}
+
+
+function update(event){
+    stage.update();
+}
+
+
